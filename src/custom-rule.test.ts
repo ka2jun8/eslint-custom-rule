4d2a76b7c77af2b7eb1293e1d2f0c7e4740b4c1a
ka2jun8 <ka210n8@gmail.com>
2022-09-30 10:06:11 +0900

    todo test

--- src/custom-rule.test.ts
+++ src/custom-rule.test.ts
@@ -3,6 +3,7 @@ import { RuleTester } from "eslint";
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const rule = require("./custom-rule");
 
+// TODO テスト作る
 const ruleTester = new RuleTester();
 ruleTester.run("custom-rule", rule, {
   valid: ["process.env"],
